81eea439725a0bf13ed4b36425310d354459465f
yuhorizons <105929737+yuhorizons@users.noreply.github.com>
2022-05-30 17:09:12 -0400

    Update create-recipe.js

--- js/create-recipe.js
+++ js/create-recipe.js
@@ -67,9 +67,11 @@ $(document).ready(function() {
         // that don't have link syntax into valid urls
         if (autoUrlSections.includes(id)) {
           section = linkify(section.replaceAll("'","asdfqwert").replaceAll("_","bsdfqwert").replaceAll("(","csdfqwert").replaceAll(")","dsdfqwert"))
-          section = section.replaceAll("asdfqwert","'").replaceAll("bsdfqwert","_").replaceAll("csdfqwert","(").replaceAll("dsdfqwert",")");
-          section = section.replace("notes","things to do");
+          section = section.replaceAll("asdfqwert","'").replaceAll("bsdfqwert","_").replaceAll("csdfqwert","(").replaceAll("dsdfqwert",")");          
         }
+        section = section.replace("notes","things to do");
+        section = section.replace("steps","itenary");
+        section = section.replace("based on","reference");
         
         // place the html inside its section
         $('#' + id).html(section);
