34844392a74fdd49085c595bb273a2f0698b4ee4
yuhorizonsfilm <107363365+yuhorizonsfilm@users.noreply.github.com>
2022-10-27 22:07:48 -0400

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -18,7 +18,7 @@ function linkify(str) {
 
 function linkify2(inputText) {
     //URLs starting with http://, https://, or ftp://
-    var replacePattern1 = /(\bhttps?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gi;
+    var replacePattern1 = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     var replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
     //URLs starting with www. (without // before it, or it'd re-link the ones done above)
